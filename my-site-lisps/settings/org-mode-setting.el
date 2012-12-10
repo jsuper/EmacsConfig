9d790e274c9d9480e799bc0614d15db2d1555ff4
tang ling <tangling.life@gmail.com>
2012-12-10 17:38:09 +0800

    org-mode repaire

--- my-site-lisps/settings/org-mode-setting.el
+++ my-site-lisps/settings/org-mode-setting.el
@@ -1,3 +1,6 @@
 ;;配置org-mode
+
+(require 'htmlize)
 (require 'org-install)
+;;(org-babel-do-load-languages 'org-babel-load-languages '((c . t) (java . t) (emacs-lisp . t)))
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
