006e908e74fc9719a644217e1e13d95144ee7539
Tang Ling <tangling.life@gmail.com>
2013-01-05 18:46:05 -0500

    modified base setting and org-mode

--- my-site-lisps/settings/org-mode-setting.el
+++ my-site-lisps/settings/org-mode-setting.el
@@ -3,4 +3,6 @@
 (require 'htmlize)
 (require 'org-install)
 ;;(org-babel-do-load-languages 'org-babel-load-languages '((c . t) (java . t) (emacs-lisp . t)))
+(add-hook 'org-mode-hook
+	  (lambda () (setq truncate-lines nil)))
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
