a816d5e8cf51e34e99b8837cbc8c8f7050bcb7af
Tang Ling <tangling.life@gmail.com>
2013-01-06 20:18:18 -0500

    merge windows-setting to linux

--- my-site-lisps/settings/python-mode-setting.el
+++ my-site-lisps/settings/python-mode-setting.el
@@ -1,5 +1,20 @@
 ;;loading emacs-common
 (require 'emacs-common)
 
+;;this function is used to run python script
+(defun execute-python-file()
+  "Using this function to exectute the file on current buffer"
+  (interactive)
+  (let ((temp-file-name (buffer-file-name)))
+    ;;when found the file extension is .py, then execute python file.py
+    (when (string-match "^.*\\.py" temp-file-name)
+      ;(message temp-file-name)
+      (execute-command-and-show-result "python" temp-file-name)
+      )
+    )
+  )
 
 
+(add-hook 'python-mode-hook
+	  (lambda ()
+	    (global-set-key (kbd "C-c C-e") 'execute-python-file)))
