c3d8e44a7223804b0a09fb45b3f39ea39144089e
Tang Ling <tangling.life@gmail.com>
2013-02-04 00:17:41 -0500

    modified settings for org-mode

--- my-site-lisps/settings/org-mode-setting.el
+++ my-site-lisps/settings/org-mode-setting.el
@@ -3,19 +3,35 @@
 (require 'htmlize)
 (require 'org-install)
 (require 'org-publish)
+;(require 'iimage)
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+
+;(defun org-toggle-iimage-in-org ()
+;  "display images in your org file"
+;  (interactive)
+;  (if (face-underline-p 'org-link)
+;      (set-face-underline-p 'org-link nil)
+;      (set-face-underline-p 'org-link t))
+;  (iimage-mode))
+
+(defun force-publish-all ()
+   "Force org-mode to publish all files"
+   (interactive)
+   (org-publish "note" t))
+
 
 
 ;;org-mode hook settings
 (add-hook 'org-mode-hook
 	  (lambda ()
 	    (setq truncate-lines nil)
-	    (global-set-key (kbd "<f8> p") 'org-publish)))
+	    (local-set-key (kbd "C-c C-p") 'force-publish-all)
+	    (global-set-key (kbd "<f8> p") 'org-publish-all)))
 
 (setq org-publish-project-alist
 	  '(
 	    ("org-notes"
-		 :base-directory "/home/commons/notes"
+		 :base-directory "/home/commons/repositories/bitbucket/mynotes"
 		 :publishing-directory "/home/commons/notes/publish"
 		 :base-extension "org"
 		 :recursive t
@@ -27,7 +43,7 @@
 		 :style "<link rel=\"stylesheet\" href=\"./org-style.css\" type=\"text/css\"/>"
 		 )
 		("note-static"
-		 :base-directory "/home/commons/notes"
+		 :base-directory "/home/commons/repositories/bitbucket/mynotes/"
 		 :publishing-directory "/home/commons/notes/publish"
 		 :recursive t
 		 :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|swf\\|zip\\|gz\\|txt\\|el"
